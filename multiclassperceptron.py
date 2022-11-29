b28c89001cd94307e8eabcbb15c6b8cfb0bfb02b
hlin863 <zcabhli@ucl.ac.uk>
2022-11-29 13:57:01 +0000

    Create multiclassperceptron.py
    I initialized a class for the multi-class perceptron.

---
+++ multiclassperceptron.py
@@ -0,0 +1,13 @@
+# TODO - design a class for the multiclass perceptron
+
+class MultiClassPerceptron:
+
+    def __init__(self, d):
+
+        """
+        
+        d represents the number of classes for each run
+        
+        """
+
+        self.number_of_classes = d
