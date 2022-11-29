91e1373a9d799021fd27689a86b51c270bd4c18f
hlin863 <zcabhli@ucl.ac.uk>
2022-11-29 16:18:30 +0000

    Implemented polynomial fitting structure
    Laid down the skeleton code for the polynomial fitting.

--- multiclassperceptron.py
+++ multiclassperceptron.py
@@ -6,8 +6,34 @@ class MultiClassPerceptron:
 
         """
         
-        d represents the number of classes for each run
+        @param: d represents the number of classes for each run
         
         """
 
-        self.number_of_classes = d
+        self.number_of_classes = d # initialises the number of classes
+
+        self.X_train = None # initialises a trainingset for the data.
+    
+    def train(self, X_train, y_train):
+
+        """
+        
+        @param: X_train is the training data.
+        @param: y_train is the training labels.
+
+        """
+
+        w = 0 # initialise the weight vector as 0.
+
+        self.X_train = X_train # set the training data to the training data.
+    
+    def polynomial_fitting(self, degree):
+
+        """
+        
+        @param: degree is the degree of the polynomial to be fitted.
+
+        """
+
+        # TODO - implement the polynomial fitting
+        pass
