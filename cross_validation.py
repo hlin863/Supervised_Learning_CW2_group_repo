6f02352c1aec7ad348f0175ade833b2a5b61836e
hlin863 <zcabhli@ucl.ac.uk>
2022-11-30 18:53:09 +0000

    Implemented cross-validation methods
    I implemented the cross-validation methods on the training and testing sets. Using the data, I have generated the train and test scores using the methods aforementioned.

--- cross_validation.py
+++ cross_validation.py
@@ -1,3 +1,5 @@
+import numpy as np # import the numpy library
+
 def cross_validation(x, y, p):
 
     """
@@ -6,8 +8,29 @@ def cross_validation(x, y, p):
     @param: y is the digit.
     @param: p is the degree of folding for the cross validation.
 
-    @return: the cross validation score.
+    @return: the list containing train and test data for each fold. 
     
     """
 
-    pass
+    # determine the number of entries
+    n_x = len(x)
+
+    random_indexes = np.arange(n_x) # create an array of indexes from 0 to n_x - 1
+
+    np.random.shuffle(random_indexes) # shuffle the indexes
+
+    result = [] # initialise the result as an empty list.
+
+    for test_indices in np.array_split(random_indexes, p): # iterate through the indexes in p folds
+
+        train_indices = np.setdiff1d(random_indexes, test_indices) # get the training indices
+
+        x_train, x_test = x[train_indices], x[test_indices] # get the training and test data
+
+        y_train, y_test = y[train_indices], y[test_indices] # get the training and test labels
+
+        result.append([x_train, y_train, x_test, y_test]) # append the training and test data to the result list.
+
+    return result # return the result as the list containing train and test data for each fold.
+
+    
