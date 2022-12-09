2df70adcff4bdbd8a1e4e1ca72d78421b35a5e79
hlin863 <zcabhli@ucl.ac.uk>
2022-12-09 19:01:43 +0000

    Update part-3-library.py
    Implemented the winnow algorithm for calculating the weights.

--- part-3-library.py
+++ part-3-library.py
@@ -1,3 +1,42 @@
 #######################################################################################
 ### Library to implement the algorithms: perceptron, winnow, least-squares and 1-NN ###
 #######################################################################################
+
+import numpy as np
+
+def winnow_algorithm(X, y):
+
+    """
+    
+    Provides an implementation for the winnow algorithm.
+
+    @param X: The input data.
+    @param y: The labels.
+    
+    """
+
+    # intialise a list to store the weights.
+
+    weights = []
+
+    w = 1 # initialize the weight vector to 1.
+
+    weights.append(w)
+
+    for t in range(len(X)): # iterate over the data from t = 1 to m.
+
+        # receive patterns x = {0, 1}
+        x = X[t]
+
+        # predict y_pred
+        y_pred = np.sign(np.dot(w, x))
+
+        y_target = y[t]
+
+        if y_pred != y_target: # if the prediction is wrong, update the weights.
+
+            w = w * 2 ** (y_target * x)
+
+        weights.append(w)
+
+    return weights
