05355d757537c256b0ebe8db8a6a519ef2196c74
Minep <zelong56@gmail.com>
2022-12-10 14:06:26 +0000

    refactor the code to make interface consistent 1-NN implementation

--- part-3-library.py
+++ part-3-library.py
@@ -6,98 +6,106 @@ import numpy as np # import the numpy library to perform mathematical operations
 
 import matplotlib.pyplot as plt # import the matplotlib library to plot the data.
 
-def winnow_algorithm(X_train, X_test, y_train):
+from sklearn.neighbors import KDTree
 
-    """
+class PredictorBase():
+    def __init__(self, name="") -> None:
+        self.__model_name = name
+        pass
     
-    Provides an implementation for the winnow algorithm.
+    def fit(self, X, y):
+        pass
 
-    @param X_train: The training dataset.
-    @param X_test: The testing dataset.
-    @param y: The labels.
+    def pred(self, X):
+        pass
+
+    def get_name(self):
+        return self.__model_name
+
+class Winnow(PredictorBase):
+    def __init__(self) -> None:
+        super().__init__("Winnow")
     
-    """
+    def fit(self, X, y):
+        weights = np.ones((X.shape[1])) # initialize the weight vector to 1.
 
-    # intialise a list to store the weights.
+        self.__n = X.shape[1] # find the number of dimensions.
 
-    weights = np.ones((X_train.shape[1])) # initialize the weight vector to 1.
+        # Training loop
+        for i in range(X.shape[0]): # iterate over the training data.
 
-    n = X_train.shape[1] # find the number of dimensions.
+            x_i, y_i = X[i], y[i] # receive patterns x = {0, 1} and y = {0, 1}.
 
-    # Training loop
-    for i in range(X_train.shape[0]): # iterate over the training data.
+            y_i_hat = -1 if weights @ x_i < n else 1 # predict the label.
 
-        x_i, y_i = X_train[i], y_train[i] # receive patterns x = {0, 1} and y = {0, 1}.
+            if (y_i_hat != y_i): # if the prediction is wrong, update the weights.
+                weights *= np.float_power(2, ((y_i - y_i_hat) * x_i)) # update the weights.
 
-        y_i_hat = -1 if weights @ x_i < n else 1 # predict the label.
+        self.__w = weights
 
-        if (y_i_hat != y_i): # if the prediction is wrong, update the weights.
-            weights *= np.float_power(2, ((y_i - y_i_hat) * x_i)) # update the weights.
+    def pred(self, X):
+        return np.where((X @ self.__w) < self.__n, -1, 1) # return the predicted labels.
 
-
-    return np.where(X_test @ weights < n, -1, 1) # return the predicted labels.
-
-def perceptron_algorithm(X, y):
-
-    """
+class Perceptron(PredictorBase):
+    def __init__(self) -> None:
+        super().__init__("Perceptron")
     
-    Provides an implementation for the perceptron algorithm.
+    def fit(self, X, y):
+        weights = [] # intialise a list to store the weights.
 
-    @param X: The input data.
-    @param y: The labels.
+        M = [] # initialise a list to store the margin.
+
+        w = np.zeros(len(X[0])) # initialize the weight vector to 0.
+
+        weights.append(w) # append the initial weight vector.
+
+        M.append(0) # append the initial margin.
+
+        for t in range(len(X)): # iterate over the data from t = 1 to m.
+
+            # receive patterns x = {0, 1}
+            x = X[t]
+
+            # predict y_pred
+            y_pred = np.sign(np.dot(w, x))
+
+            y_target = y[t]
+
+            # implement a mistake function to calculate the number of mistakes in the perceptron algorithm to update the weight. 
+
+            if y_pred != y_target: # if the prediction is wrong, update the weights.
+
+                w = w + y_target * x
+
+            weights.append(w)
+
+        self.__w = weights
+
+    def pred(self, X):
+        raise NotImplemented()
+
+class LeastSquare(PredictorBase):
+    def __init__(self) -> None:
+        super().__init__("Least Square")
+
+    def fit(self, X, y):
+        self.__w = np.linalg.pinv(X.T @ X) @ X.T @ y
+
+    def pred(self, X):
+        y_ = X @ self.__w
+        return np.where(y_ <= 0, -1, 1)
+
+class OneNearestNeighbor(PredictorBase):
+    def __init__(self) -> None:
+        super().__init__("1NN")
+
+    def fit(self, X, y):
+        self.__y = y
+        self.__spatial = KDTree(X)
     
-    """
-
-    weights = [] # intialise a list to store the weights.
-
-    M = [] # initialise a list to store the margin.
-
-    w = np.zeros(len(X[0])) # initialize the weight vector to 0.
-
-    weights.append(w) # append the initial weight vector.
-
-    M.append(0) # append the initial margin.
-
-    for t in range(len(X)): # iterate over the data from t = 1 to m.
-
-        # receive patterns x = {0, 1}
-        x = X[t]
-
-        # predict y_pred
-        y_pred = np.sign(np.dot(w, x))
-
-        y_target = y[t]
-
-        # implement a mistake function to calculate the number of mistakes in the perceptron algorithm to update the weight. 
-
-        if y_pred != y_target: # if the prediction is wrong, update the weights.
-
-            w = w + y_target * x
-
-        weights.append(w)
-
-    return weights
-
-
-def least_squares_algorithm(y_pred, y):
-
-    """
-
-    Provides an implementation for the least squares algorithm.
-
-    @param y_pred: The predicted labels.
-
-    @param y: The labels.
-
-    """
-
-    # find the difference between y_pred and y squared.
-    difference_squared = (y_pred - y) ** 2
-
-    # find the minimal difference between y_pred and y squared.
-    min_difference_squared = np.min(difference_squared)
-
-    return min_difference_squared    
+    def pred(self, X):
+        indices = self.__spatial.query(X, k=1, return_distance=False)
+        return self.__y[indices.ravel()].reshape(-1,1)
 
 def mistake(y_pred, y_target):
 
@@ -109,10 +117,9 @@ def mistake(y_pred, y_target):
     @param y_target: The target labels.
     
     """
+    Ierror = y_pred != y_target
 
-    pass
-
-classifiers = ["perceptron", "winnow", "least-squares", "1-NN"] # create a list to store the classifiers.
+    return np.sum(Ierror) / y_pred.shape[0]
 
 
 def plot_complexity(classifier_function, dimensions, samples_per_dimension):
@@ -169,46 +176,67 @@ def train_test_split(data):
 
     return data[:int(len(data) * 0.8)], data[int(len(data) * 0.8):] # return the training and testing data.
 
-errors_by_dimension = np.zeros(100) # create an array to store the errors by dimension.
+models = np.array([Winnow(), LeastSquare(), OneNearestNeighbor()])
 
-for n in range(1, 100):
+M = np.empty((len(models), 100)) # create an array to store the errors by dimension.
+M[:] = np.nan
+
+for n in range(1, 101):
 
     generalization_error = float('inf') # initialize the generalization error.
 
     m = 1 # initialize the number of samples.
 
-    while m <= 40:
+    # Approximate 2^n by maclaurin expansion up to x^2.
+    # MC: We use this to sample the population \mathcal{X} to approximate expectation
+    # TODO analyse the bias and try to incorporate importance weighting.
+    N = int(n * np.log(2))
+    L = int(1 + N + N * N / 4)
+    masked = np.zeros(len(models), dtype=bool)
 
-        X, y = sample_data(m, n) # sample the data.
+    print("RUNNING: n = %0.3d / 100"%(n), end='\r')
 
-        X_train, X_test = train_test_split(X) # split the data into training and testing data.
+    while m <= 100:
 
-        y_train, y_test = train_test_split(y) # split the labels into training and testing labels.
+        # No need for train test spilt, as we have direct access to D
+        # we can simply drawn from D for any novel test points.
+        X_test, y_test = sample_data(L, n)
+        X, y = sample_data(m, n) # sample S_m
 
-        y_pred = winnow_algorithm(X_train, X_test, y_train) # predict the labels using the winnow algorithm.
-
-        n_errors = np.sum(y_pred != y_test) # find the number of errors.
-
-        print("Number of errors", n_errors)
-
-        generalization_error = n_errors / len(y_test) # calculate the generalization error.
-
-        print("Generalization error", generalization_error)
-
-        if generalization_error <= 0.1: # if the generalization error is less than or equal to 0.1, allocate the value to the array.
-
-            errors_by_dimension[n] = generalization_error # store the generalization error.
-
+        # no need to proceed no classifier is pending
+        if np.prod(masked) == 1:
             break
             
+        for i, model in enumerate(models):
+            if masked[i]:
+                continue
+
+            model.fit(X, y)
+
+            y_pred = model.pred(X_test) # predict the labels using the winnow algorithm.
+
+            generalization_error = mistake(y_pred, y_test) # calculate the generalization error.
+
+            if generalization_error <= 0.1: # if the generalization error is less than or equal to 0.1, allocate the value to the array.
+                
+                # mark this classifier as ok
+                masked[i] = True
+                M[i, n - 1] = m # store the generalization error.
+
+                break
+            
         m += 1 # increment the number of samples.
+print()
 
-plt.plot(range(100), errors_by_dimension, label="winnow") # plot the errors by dimension.
+for i in range(M.shape[0]):
+    model = models[i]
+    name = model.get_name()
 
-plt.xlabel("Dimensions") # set the x-axis label.
+    plt.plot(range(100), M[i,:], label=name) # plot the errors by dimension.
 
-plt.ylabel("Generalization error") # set the y-axis label.
-
-plt.savefig("Part-3-images/generalization_error.png") # save the plot as a png file.
+plt.xlabel("Dimensions (n)") # set the x-axis label.
+plt.ylabel("Sample Size (m)") # set the y-axis label.
+plt.legend()
+plt.savefig("Part-3-images/plot_complexity.png") # save the plot as a png file.
 
 print("SUCCESS!")
