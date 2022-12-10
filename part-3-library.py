fe5e73ded23561048870c3b2a97085328dc8c28f
hlin863 <zcabhli@ucl.ac.uk>
2022-12-10 10:45:32 +0000

    Update part-3-library.py
    Implemented an algorithm to calculate the number of errors between y-pred and y-test where y-pred is calculated using the winnow algorithm.

--- part-3-library.py
+++ part-3-library.py
@@ -6,42 +6,36 @@ import numpy as np # import the numpy library to perform mathematical operations
 
 import matplotlib.pyplot as plt # import the matplotlib library to plot the data.
 
-def winnow_algorithm(X, y):
+def winnow_algorithm(X_train, X_test, y_train):
 
     """
     
     Provides an implementation for the winnow algorithm.
 
-    @param X: The input data.
+    @param X_train: The training dataset.
+    @param X_test: The testing dataset.
     @param y: The labels.
     
     """
 
     # intialise a list to store the weights.
 
-    weights = []
+    weights = np.ones((X_train.shape[1])) # initialize the weight vector to 1.
 
-    w = 1 # initialize the weight vector to 1.
+    n = X_train.shape[1] # find the number of dimensions.
 
-    weights.append(w)
+    # Training loop
+    for i in range(X_train.shape[0]): # iterate over the training data.
 
-    for t in range(len(X)): # iterate over the data from t = 1 to m.
+        x_i, y_i = X_train[i], y_train[i] # receive patterns x = {0, 1} and y = {0, 1}.
 
-        # receive patterns x = {0, 1}
-        x = X[t]
+        y_i_hat = 0 if weights @ x_i < n else 1 # predict the label.
 
-        # predict y_pred
-        y_pred = np.sign(np.dot(w, x))
+        if (y_i_hat != y_i): # if the prediction is wrong, update the weights.
+            weights *= np.float_power(2, ((y_i - y_i_hat) * x_i)) # update the weights.
 
-        y_target = y[t]
 
-        if y_pred != y_target: # if the prediction is wrong, update the weights.
-
-            w = w * 2 ** (y_target * x)
-
-        weights.append(w)
-
-    return weights
+    return np.where(X_test @ weights < n, 0, 1) # return the predicted labels.
 
 def perceptron_algorithm(X, y):
 
@@ -182,5 +176,15 @@ for n in range(100):
         X, y = sample_data(m, n) # sample the data.
 
         X_train, X_test = train_test_split(X) # split the data into training and testing data.
+
+        y_train, y_test = train_test_split(y) # split the labels into training and testing labels.
+
+        # y_pred = winnow_algorithm(X_train, X_test, y_train) # predict the labels using the winnow algorithm.
+
+        y_pred = winnow_algorithm(X_train, X_test, y_train) # predict the labels using the winnow algorithm.
+
+        n_errors = np.sum(y_pred != y_test) # find the number of errors.
+
+        print("Number of errors: ", n_errors) # print the number of errors.
 
 print("SUCCESS!")
